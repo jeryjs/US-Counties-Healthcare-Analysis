989625416bc2b2a034577f315b06eafa5eed360d
Jery <jery99961@gmail.com>
2025-08-27 09:26:12 +0530

    chore: simplify base path configuration for deployment

--- results/vite.config.js
+++ results/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isGHPages = process.env.GH_DEPLOY == true;
+// const isGHPages = process.env.GH_DEPLOY == false;
 
 export default defineConfig({
   plugins: [react()],
-  base: isGHPages ? '/US-Counties-Healthcare-Analysis/' : '/',
+  base: '/US-Counties-Healthcare-Analysis/',
 })
